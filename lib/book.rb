09f2588f41a59f6a75061bb71f076a87d1e4cca5
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 23:25:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
+
   def initialize(title)
     @title = title
   end
