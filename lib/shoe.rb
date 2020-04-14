6a8fd5296261d90f3c42a7049b387fb56ce0fb0e
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 00:00:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = [] 
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -14,7 +14,7 @@ class Shoe
   end
 
   def brand=(brand)
-    BRANDS << brand 
+    BRANDS << brand
   end
 
 end
